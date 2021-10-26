fe290fa032f8c80e202ed815c9d33f75527b778e
ronyman.com <47080351+ronyman-com@users.noreply.github.com>
2021-10-26 15:46:39 +1100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -68,7 +68,7 @@ html_theme_options = {
     'logo': 'webpres-logo.png',
     'description': 'An opnen source cms site builder.',
     'display_version': True,
-    'logo_only':True,
+    #'logo_only':True,
     'logo_name':True,
     'show_powered_by': True,
     'github_user': 'WebPres-org',
