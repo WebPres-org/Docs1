1c27f37b1026c3fab6a00f69eabd031f27021b49
ronyman.com <47080351+ronyman-com@users.noreply.github.com>
2021-10-26 12:30:04 +1100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -61,7 +61,7 @@ html_theme_options = {
     'logo_only':True,
     'show_powered_by': False,
     'github_user': 'WebPres-org',
-    'github_repo': 'Docs',
+    'github_repo': 'Docs/docs',
     'github_banner': True,
     'show_related': False,
     'note_bg': '#FFF59C'
